26ff6f2222aa4d0090d7ed43c1705a99e829c2f8
StalinAM <sta_vla411@hotmail.com>
2024-01-11 17:57:49 -0500

    add styles custom

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,33 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin')
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {}
+  },
+  plugins: [
+    plugin(function ({ addComponents }) {
+      addComponents({
+        '.label': {
+          position: 'absolute',
+          top: '0',
+          right: '0',
+          bottom: '0',
+          margin: 'auto',
+          content: "''",
+          borderRadius: '50%',
+          display: 'block',
+          width: '20rem',
+          height: '20rem',
+          left: '-5rem',
+          textAlign: 'center',
+          transition: 'box-shadow 0.5s ease-out',
+          zIndex: '-1'
+        },
+        '.check': {
+          boxShadow: 'inset 0 0 0 10em #e7ecee'
+        }
+      })
+    })
+  ]
 }
